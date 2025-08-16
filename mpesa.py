5b8cac85f62998850a813c221d47e3f49c65ce4e
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-08-16 09:41:02 +0300

    updated callback/mpesa.py

--- mpesa.py
+++ mpesa.py
@@ -62,7 +62,7 @@ class Mpesa:
             "PartyA": phone,
             "PartyB": self.business_short_code,
             "PhoneNumber": phone,
-            "CallBackURL": "https://517c60e3498b.ngrok-free.app/payments/callback",
+            "CallBackURL": "https://mpesaapi-backendmashcollection.onrender.com/payments/callback",
             "AccountReference": "Test",
             "TransactionDesc": desc,
         }
